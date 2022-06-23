ba727638c35486e8e2f6fc0b190a62ac73cbe407
Marage <76840739+MarageDev@users.noreply.github.com>
2022-06-23 05:23:36 +0200

    Added compression logic

--- js/VideoCompressor.js
+++ js/VideoCompressor.js
@@ -6,21 +6,54 @@ let videoSize = document.querySelector('#videoSize');
 let videoLength = document.querySelector('#videoLength');
 let videoResolution = document.querySelector('#videoResolution');
 let btnCompressFile = document.querySelector('#btnCompressFile')
+let btnOutputVideoPath = document.querySelector('#btnBrowseOutputfile')
 
+//    PRESETS OPTIONS
+let EightMbOptionSelector = document.querySelector('#EightMbOptionSelector')
+let FiftyMbOptionSelector = document.querySelector('#FiftyMbOptionSelector')
+let HundredMbOptionSelector = document.querySelector('#HundredMbOptionSelector')
+
+
+//    TOP BAR BUTTONS
 let btnClose = document.querySelector('#close')
 let btnMinimize = document.querySelector('#minimize')
 
-
+//    REQUIRES
 const {ipcRenderer} = require('electron')
 const ipc = ipcRenderer
 const { PythonShell} = require("python-shell")
-    
+
+
+const spawner = require('child_process').spawn;
+let python_process = spawner('python', ['./main.py'])
+function sendEvent (name, args){
+   // Define the array and add the default values
+   spawnerArgs = ['./main.py', `${name}`]
+
+   // Check if the event contains arguments
+   if(typeof args  !== 'undefined' ){
+       // Add every arguments to the array "spawnerArgs"
+       args.forEach(element => {
+           spawnerArgs.push(element)
+       });
+   }else{
+       // If it doesn't contain any arguments, do nothing
+       console.log("Doesn't possess args")
+   }
+
+   // Add the event and its arguments from "spawnerArgs" and the receive part
+   python_process = spawner('python', spawnerArgs)
+   python_process.stdout.on('data', (data) => {
+       console.log('Data received from python :', data.toString())
+   })
+}
+
+
+
 
 let inputVideoPath   = ""
 let outputVideoPath  = ""
 let targetVideoSize  = ""
-
-
 
 
 
@@ -55,6 +88,8 @@ btnVideoPathBrowse.addEventListener('click', () =>{
     }
     input.click();
 })*/
+
+
 
 btnClose.addEventListener('click', () =>{
    ipc.send('closeApp')
@@ -98,24 +133,74 @@ function openInputFileExplorer(){
     input.click();
     console.log('a')
 }
+
+function openOutputFileExplorer(){
+   let outputVideoTextBar = document.querySelector('#outputVideoTextBar');
+   test.innerHTML = "opened the output browse"
+
+    var input1 = document.createElement('input');
+    input1.type = 'file';
+    input1.accept = '' //why not
+    input1.onchange = e => { 
+
+       // getting a hold of the file reference
+       var file = e.target.files[0]; 
+
+       // setting up the reader
+       var reader = new FileReader();
+       reader.readAsText(file,'UTF-8');
+
+       // here we tell the reader what to do when it's done reading...
+       reader.onload = readerEvent => {
+          var content = readerEvent.target.result; // this is the content!
+          console.log( content );
+          outputVideoTextBar.value = file.path;
+       }
+    }
+    input1.click();
+}
+
 function closeWindowFunction(){
    ipc.send('closeApp')
    let test = document.querySelector('#test');
    test.innerHTML = "Test before require";
 }
+function updateOutputSizeText(value){
+   let outputSizeSlider = document.querySelector('#outputSizeSlider');
+   let outputSizeText = document.querySelector('#outputSizeText');
+   outputSizeText.innerHTML = outputSizeSlider.value.toString()
+}
+
+var outputSizeSlider = document.querySelector('#outputSizeSlider');
+var outputSizeText = document.querySelector('#outputSizeText');
+
+outputSizeSlider.onchange = function(){
+   outputSizeText.value = this.value;
+}
+outputSizeText.onchange = function(){
+   outputSizeSlider.value = outputSizeText.value
+}
+
+
 function compress(){
    let test = document.querySelector('#test');
    test.innerHTML = "Test before require";
    
-   
+   let inputVideoTextBar = document.querySelector('#inputVideoTextBar');
+   let outputVideoTextBar = document.querySelector('#outputVideoTextBar');
+   let outputSizeSlider = document.querySelector('#outputSizeSlider');
+   let outputSizeText = document.querySelector('#outputSizeText');
    test.innerHTML = "Test after require";
-   
+   inputVideoPath = inputVideoTextBar.value
+   outputVideoPath = outputVideoTextBar.value
+   outputSize = outputSizeText.value
+
    
    let options = {
       mode: 'text',
       pythonOptions: ['-u'],
       scriptPath: './python/',
-      args: ['value1', 'value2', 'value3']
+      args: [inputVideoPath, outputVideoPath, outputSize]
    };
    
    PythonShell.run('compress.py', options, function (err, results) {
@@ -123,6 +208,7 @@ function compress(){
       console.log('results: %j', results);
       
    }) 
+   // sendEvent('CreateNewCustomFile', ['Magic Note','This is the event named : ','CreateNewCustomFile'])
 }
 
 /*
--- python/compress.py
+++ python/compress.py
@@ -1,10 +1,8 @@
 import os, ffmpeg
 import python_shell as pys
 import sys
-
+args = sys.argv[1:]
 def compress_video(video_full_path, output_file_name, target_size):
-
-
     min_audio_bitrate = 32000
     max_audio_bitrate = 256000
 
@@ -30,12 +28,6 @@ def compress_video(video_full_path, output_file_name, target_size):
     
     ffmpeg.output(i, os.devnull,**{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}).overwrite_output().run()
     ffmpeg.output(i, output_file_name,**{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}).overwrite_output().run()
-input_video = "../2022-06-19 22-44-35"
-output_video_name = "../b"
-output_target_file_size = 10
-args = sys.argv[1:]
-print(args)
-compress_video(f'{input_video}.mp4', f'{output_video_name}.mp4', float(output_target_file_size)*1000)
-#compress_video(f'{input_video}.mp4', f'{output_video_name}.mp4', float(output_target_file_size)*1000)
-print(f'The video {input_video} will be compressed to a size of {output_target_file_size} as {output_video_name}')
 
+
+compress_video(args[0], args[1],float(args[2])*1000)
